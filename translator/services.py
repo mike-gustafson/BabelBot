f3d750249fe04df3aad59bc8117298f0e4666d2f
Mike Gustafson <gustafson.m@gmail.com>
2025-04-13 03:06:52 -0700

    Remove async from translate_text function and update translation call

--- translator/services.py
+++ translator/services.py
@@ -1,7 +1,6 @@
-import asyncio
 from googletrans import Translator
 
-async def translate_text(text, dest_lang='en'):
+def translate_text(text, dest_lang='en'):
     translator = Translator()
-    translation = await translator.translate(text, dest=dest_lang)
+    translation = translator.translate(text, dest=dest_lang)
     return translation.text
