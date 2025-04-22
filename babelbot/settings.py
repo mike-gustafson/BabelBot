6e4b5dd114e51665053e8fbe64e0aef4e99bb72f
Mike Gustafson <93888281+mike-gustafson@users.noreply.github.com>
2025-04-21 19:22:08 -0700

    Update SECRET_KEY handling in settings.py to use environment variable
    - Modified the SECRET_KEY assignment to retrieve the value from an environment variable, enhancing security by allowing for different configurations in production and development environments.

--- babelbot/settings.py
+++ babelbot/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-change-this-1234"
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-change-this-1234')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if not "ON_HEROKU" in os.environ:
