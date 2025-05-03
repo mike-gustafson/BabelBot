c8c1e6752beedc67e8abc557727765030a5bcaf3
Mike Gustafson <93888281+mike-gustafson@users.noreply.github.com>
2025-05-03 16:58:29 -0700

    Refactor URL handling in JavaScript and templates: Replace hardcoded URLs with Django template tags for OCR and translation processes, enhancing maintainability. Update OCR app's URL configuration to include app name for better namespacing.

--- ocr/urls.py
+++ ocr/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'ocr'
+
 urlpatterns = [
     path('tech-demo/', views.tech_demo, name='ocr_tech_demo'),
     path('process/', views.perform_ocr, name='perform_ocr'),
